01bb122f7d3c488e1f2855a67b56cc034ba9ce47
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-10-14 12:48:06 -0400

    Working on exercise 1.9

---
+++ part1/complex-state/src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    }
+
+    console.log(allClicks);
+  
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+
+
+
+// Next section of code!!
+// const Display = props => <div>{props.value}</div>
+
+// const Button = (props) => {
+//   <button onClick={props.handleClick}>
+//     {props.text}
+//   </button>
+// }
+
+// const App = (props) => {
+//   const [value, setValue] = useState(10)
+
+//   const setToValue = (newValue) => {
+//     setValue(newValue)
+//   }
+
+
+//   return (
+//     <div>
+//       <Display value={value} />
+//       <Button handleClick={() => setToValue(1000)} text="thousand" />
+//       <Button handleClick={() => setToValue(0)} text="reset" />
+//       <Button handleClick={() => setToValue(value + 1)} text="increment" />
+//     </div>
+//   )
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+export default App
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+}
+  return (
+    <div>
+      {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
 const Statistics = (props) => {
@@ -13,24 +30,35 @@ const Statistics = (props) => {
     <Button handleClick={() => props.setNeutral(props.neutral + 1)} text="neutral" />
     <Button handleClick={() => props.setBad(props.bad + 1)} text="bad" />
     <h1>statistics</h1>
+    <History allClicks={props.allClicks} />
     <p>good {props.good}</p>
     <p>neutral {props.neutral}</p>
     <p>bad {props.bad}</p>
     <p>all {props.good + props.neutral + props.bad}</p>
     <p>average {(props.good + props.neutral + props.bad) / 3}</p>
-    <p>positive {(props.good / (props.good + props.neutral + props.bad)) * 100}%</p>
+    <p>positive {(props.good / (props.good + props.neutral + props.bad)) * 100 + ` %`}</p>
   </div>
   )
 }
 
-const App = (props) => {
+const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
   
   return (
-   <Statistics />
+   <Statistics
+     good={good}
+     setGood={setGood}
+     neutral={neutral}
+     setNeutral={setNeutral}
+     bad={bad}
+     setBad={setBad}
+     allClicks={allClicks}
+     setAll={setAll}
+   />
   )
 }
 
