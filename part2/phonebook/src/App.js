5b9fb1204d25adfedf78dde788dd4d177adcd8bb
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-12-15 22:07:23 -0500

    Working on the map function on lines 49 & 50. Setting up new files to separate components later on.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,32 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ])
+  const [ persons, setPersons ] = useState([{ name: 'Arto Hellas' }]) 
   const [ newName, setNewName ] = useState('')
+  // const [ showAll, setShowAll ] = useState(true)
 
-  // const addName = (event) => {
-  //   event.preventDefault()
-  //   const noteObject = {
-  //     content: newNote,
-  //     id: persons,
-  //   }
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  // const namesToShow = showAll
+  //   ? persons
+  //   : persons.filter(person => person.important === true)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
+        <div>filter shown with<input /></div>
+        <h3>Add a new</h3>
         <div>
-          name: <input />
+          name: <input
+          value={newName}
+          onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.map(newName =>
+          <p key={newName.id} person={newName} />
+        )}
+      </ul>
     </div>
   )
 }
+
+// alert(`${newName} is already added to phonebook`)
 
 export default App
