27f6f88d59bf7e2b6e40a69ff57a5b17cb8a0d13
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-03-11 19:06:57 -0500

    Rendering whole list of countries for now and working through some of section two part D.

--- part2/render-collections/src/App.js
+++ part2/render-collections/src/App.js
@@ -5,7 +5,7 @@ import Note from './components/Note'
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
-  const [showAll ] = useState(true)
+  const [showAll, setShowAll ] = useState(true)
 
   useEffect(() => {
     console.log('effect')
@@ -38,6 +38,16 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
@@ -45,9 +55,18 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note, i) =>
+          <Note
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
