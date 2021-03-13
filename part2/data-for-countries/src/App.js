ef0c80106c48158a4a289d6d0f7432470704dda9
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-03-13 18:37:48 -0500

    Adding functionality to search for and get results for countries

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 function App() {
   const [ countries, setCountries ] = useState([])
+  const [ filteredCountries, setFilteredCountries ] = useState([])
   const [ search, setSearch ] = useState('')
 
   useEffect(() => {
@@ -19,14 +20,24 @@ function App() {
       })
   }, [])
 
+  useEffect(() => {
+    console.log(`Searching for ${search}`)
+
+    const filteredCountryList = countries.filter(({ name, altSpellings }) =>
+                                name.toLowerCase().includes(search.toLowerCase() )||
+                                altSpellings.some(spelling => spelling.toLowerCase().includes(search.toLowerCase() ))
+      )
+      setFilteredCountries(filteredCountryList)
+  }, [search, countries])
+
   const handleOnChange = (event) => {
     setSearch(event.target.value)
   }
   
-  const filterCountries = countries.filter(country => country.name.toLowerCase().includes( search.toLowerCase() )).map(filteredCountries => (
-    <div>{filteredCountries.name}</div>
-  ))
-  console.log(filterCountries);
+  // const filterCountries = countries.filter(country => country.name.toLowerCase().includes( search.toLowerCase() )).map(filteredCountries => (
+  //   <div>{filteredCountries.name}</div>
+  // ))
+  // console.log(filterCountries);
   // const displayCountries = filterCountries.map((country, index) => <div key={index}>{country.name}</div>)
 
   return (
@@ -37,9 +48,31 @@ function App() {
           onChange={(event) => {handleOnChange(event)}}
         />
       </div>
-      <div>
-        {filterCountries}
-      </div>
+      { filteredCountries.length >= 10 &&
+          <div>
+            Too many matches, specify another filter
+          </div>
+      }
+      { filteredCountries.length > 1 && filteredCountries.length <= 10 &&
+          <div>
+            {filteredCountries.map(country => (<div>{country.name}</div>))}
+          </div>
+      }
+      { filteredCountries.length === 1 && filteredCountries.map(country => 
+                <div>
+                  <h2>{country.name}</h2>
+                  <div>Capital {country.capital}</div>
+                  <div>Population {country.population}</div>
+                  <h3>Languages</h3>
+                  {filteredCountries[0].languages.map(languages =>
+                    <li>
+                      <ul>{languages.name}</ul>
+                    </li>
+                  )}
+                  <img src={country.flag} alt={`flag of ${country}`} />
+                </div>
+                )      
+      }
     </div>
   );
 }
